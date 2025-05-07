bf79cce8d529307b202e741e956e82fc3e463705
silpako <silpako167@gmail.com>
2025-05-07 17:26:43 +0530

    color change

--- lib/features/auth/presentation/pages/login_screen.dart
+++ lib/features/auth/presentation/pages/login_screen.dart
@@ -20,6 +20,7 @@ class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: AppColors.white,
       body: SingleChildScrollView(
         child: Column(
           children: [
